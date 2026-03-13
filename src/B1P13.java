e30c03dfe0acc04c057910d59be5a3636000ee85
laxmimishra291 <laxmimishra29102005@gmail.com>
2026-03-13 14:45:01 +0530

    UC1: Add welcome message module

--- src/B1P13.java
+++ src/B1P13.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+/**
+ * Hotel Booking Application
+ * Use Case 1: Application Entry & Welcome Message
+ *
+ * @author Laxmi
+ * @version 1.0
+ */
 
 public class B1P13 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Book My Stay App");
+        System.out.println("Hotel Booking System v1.0");
+        System.out.println("Application started successfully!");
+    }
 }
