c02c85827a249eefdb7193e9942b77f76358f435
laxmimishra291 <laxmimishra29102005@gmail.com>
2026-03-13 14:56:53 +0530

    UC2:Add welcome message module

--- src/B1P13.java
+++ src/B1P13.java
@@ -1,17 +1,85 @@
 /**
- * Hotel Booking Application
- * Use Case 1: Application Entry & Welcome Message
+ * Book My Stay App
+ * Use Case 2: Basic Room Types & Static Availability
+ *
+ * Demonstrates abstraction, inheritance, polymorphism,
+ * and simple room availability representation.
  *
  * @author Laxmi
- * @version 1.0
+ * @version 2.1
  */
 
+// Abstract class
+abstract class Room {
+
+    protected int beds;
+    protected double size;
+    protected double price;
+
+    public Room(int beds, double size, double price) {
+        this.beds = beds;
+        this.size = size;
+        this.price = price;
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("Beds: " + beds);
+        System.out.println("Size: " + size + " sq.ft");
+        System.out.println("Price: ₹" + price);
+    }
+}
+
+// Single Room
+class SingleRoom extends Room {
+
+    public SingleRoom() {
+        super(1, 120, 2000);
+    }
+}
+
+// Double Room
+class DoubleRoom extends Room {
+
+    public DoubleRoom() {
+        super(2, 200, 3500);
+    }
+}
+
+// Suite Room
+class SuiteRoom extends Room {
+
+    public SuiteRoom() {
+        super(3, 350, 7000);
+    }
+}
+
+// Main Class
 public class B1P13 {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Book My Stay App");
-        System.out.println("Hotel Booking System v1.0");
-        System.out.println("Application started successfully!");
+        System.out.println("Book My Stay App");
+        System.out.println("Hotel Booking System v2.1");
+        System.out.println("--------------------------");
+
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        int singleAvailability = 5;
+        int doubleAvailability = 3;
+        int suiteAvailability = 2;
+
+        System.out.println("\nSingle Room Details:");
+        single.displayRoomDetails();
+        System.out.println("Available: " + singleAvailability);
+
+        System.out.println("\nDouble Room Details:");
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available: " + doubleAvailability);
+
+        System.out.println("\nSuite Room Details:");
+        suite.displayRoomDetails();
+        System.out.println("Available: " + suiteAvailability);
     }
 }
